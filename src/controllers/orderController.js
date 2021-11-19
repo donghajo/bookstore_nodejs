6030c9581fbd0e72d6ed40145075b7db3be0009f
jodongha <66076070+jodongha@users.noreply.github.com>
2021-11-20 02:49:14 +0900

    no message

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,6 +1,7 @@
 const orderService = require('../services/orderService');
 const mypageService = require('../services/mypageService');
 const bookService = require('../services/bookService');
+const { NotExtended } = require('http-errors');
 
 
 exports.orderPage = async(req, res) => {
@@ -22,31 +23,39 @@ exports.orderPage = async(req, res) => {
     }
 }
 
-exports.orderSingleItem = async(req, res) => {
+exports.orderSingleItem = async(req, res, next) => {
     const {book_id} = req.params.book_id;
     const {session} = req.session;
     const count = [req.body.amount];
     try{
-        const cardInfo = mypageService.cardInfo(session);
-        const addressInfo = mypageService.addressInfo(session);
-
+        const cardInfo = await mypageService.cardInfo(session);
+        const addressInfo = await mypageService.addressInfo(session);
+        const orderBook = await bookService.getDetail(session);
         for(let i = 0; i <count.length; i++){
             if(count[i]<=0){
                 res.send('<script type="text/javascript">alert("도서 수량을 똑바로 적어주세요.");</script>');
                 return err;
             }
         }
+        req.body.cardInfo = cardInfo;
+        req.body.addressInfo = addressInfo;
+        req.sesion.orderBook = orderBook;
+        req.sesison.count = count;
 
-
+        next()
     }catch(err){
         return res.status(500).json(err);
     }
 }
 
+
+exports.purchase = async(req,res,next) =>{
+    const user = req.session
+}
 exports.getOrderList = async(req, res) => {
-    
+    return res.render('orderList');
 }
 
 exports.getOrderDetail = async(req, res) =>{
-
+    return res.render('orderDetail');
 }
