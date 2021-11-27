743b464be39cc2ce17e983b93a74478cc4350999
jodongha <66076070+jodongha@users.noreply.github.com>
2021-11-27 16:48:11 +0900

    추천인

--- src/queries/signQuery.js
+++ src/queries/signQuery.js
@@ -3,3 +3,10 @@ exports.signUp = 'insert into user (user_id, user_pw, user_name) values(?, ?, ?)
 
 //signIn query
 exports.signIn = 'select * from user where user_id=? and user_pw=?'
+
+//recommender query
+exports.recommend = 'insert into user (user_id, user_pw, user_name, recommender_id) values(?, ?, ?,?)';
+
+exports.insertPoint = 'update user set point = (point + 10000) where user_id = ?'; 
+
+exports.getPoint = 'select * from user where user_id=?';
